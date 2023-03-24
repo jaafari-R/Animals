055829f6db38b096e0e64d31884766b27768e834
potato <jaafariramadan@gmail.com>
2023-03-25 01:48:42 +0300

    declared vertibrates class

---
+++ types/vertebrates.h
@@ -0,0 +1,19 @@
+#ifndef VERTEBRATES_H
+#define VERTEBRATES_H
+
+#include "../animal.h"
+
+/**
+ * Animals with a backbone are Vertebrates
+*/
+class Vertebrate : public Animal
+{
+public:
+    Vertebrate() = default;
+    Vertebrate(std::string_view description);
+    virtual ~Vertebrate();
+
+private:
+};
+
+#endif
